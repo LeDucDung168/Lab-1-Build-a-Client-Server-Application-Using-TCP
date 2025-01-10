89ae4318179f15b2b3a9fe56a718c7174a327b54
Duc Le Dung <DungLDHE173255@fpt.edu.vn>
2025-01-10 22:49:59 +0700

    pp

---
+++ LAB 1 TCP/ServerApp/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void ProcessMessage (object parm)
+    {
+
+    }
+    static void ExecuteServer(string host, int port)
+    {
+        
+    }
+    public static void Main()
+    {
+        string host = "127.0.0.1";
+        int port = 1234;
+        ExecuteServer(host, port);
+    }
+}
